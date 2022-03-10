717e2703ba67444905321141c69ae4b1fb45be50
Parker Stephenson <parkerstephenson00@gmail.com>
2022-03-10 01:16:18 -0800

    Update hungryhelperdatabase.sql
    finished the main tabled and practiced inputting recipes

--- hungryhelperdatabase.sql
+++ hungryhelperdatabase.sql
@@ -1,10 +1,14 @@
 USE hungryhelpers;
+
 CREATE TABLE Recipe (
-  RecipeID     BIGINT UNSIGNED NOT NULL,
+  RecipeID     INT NOT NULL AUTO_INCREMENT,
   RecipeName         VARCHAR(50),
   RecipePrepTimeMins	INT,
   RecipeCookTimeMins	INT,
   RecipeCals		INT,
+  RecipeCarbs		INT,
+  RecipeProtein		INT,
+  RecipeFats		INT,
   RecipeSkillLevel	VARCHAR(15),
   RecipePicture 	BLOB,
   CuisineType	VARCHAR(40),
@@ -13,9 +17,32 @@ CREATE TABLE Recipe (
   PRIMARY KEY (RecipeID)
   );
 
-  CREATE TABLE Ingredients (
-  RecipeIngredientID BIGINT UNSIGNED NOT NULL,
-  RecipeID			BIGINT UNSIGNED NOT NULL,
-  PRIMARY KEY (RecipeIngredientID),
+ CREATE TABLE RecipeIngredients (
+  RecipeIngredientsID INT NOT NULL AUTO_INCREMENT,
+  RecipeID			INT,
+  IngredientAmount  VARCHAR(30),
+  IngredientPrepInfo VARCHAR(50),
+  PRIMARY KEY (RecipeIngredientsID),
   FOREIGN KEY (RecipeID) REFERENCES Recipe(RecipeID)
 );
+
+CREATE TABLE Ingredient (
+  IngredientID INT NOT NULL AUTO_INCREMENT,
+  RecipeIngredientsID	INT,
+  IngredientName VARCHAR(64),
+  IngredientDescription VARCHAR(64),
+  IngredientPrice FLOAT,
+  PRIMARY KEY (IngredientID),
+  FOREIGN KEY (RecipeIngredientsID) REFERENCES RecipeIngredients(RecipeIngredientsID)
+
+  );
+   
+  
+  INSERT INTO Recipe (RecipeName)
+  VALUES ('Fish and Rice');
+  
+  INSERT INTO Recipe (RecipeName)
+  VALUES ('Cheese'), ('Mayo');
+  
+  SELECT * FROM Recipe;
+  
