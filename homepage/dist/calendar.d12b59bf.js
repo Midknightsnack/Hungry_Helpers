90270aeaa907c795fc6ab41f6b38a12c321852a1
Bradley Diep <bdippin12@csu.fullerton.edu>
2022-03-28 19:49:55 -0700

    update

--- homepage/dist/calendar.d12b59bf.js
+++ homepage/dist/calendar.d12b59bf.js
@@ -379,7 +379,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62016" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63950" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
