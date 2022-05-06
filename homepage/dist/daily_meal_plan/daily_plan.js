93e00c35c378709dc3304536bcdb7cc9949de229
Parker Stephenson <parkerstephenson00@gmail.com>
2022-05-05 18:52:12 -0700

    Fixed some of the daily meal plan
    Still lots of work to do for it, but the barebones daily meal planner is now fully integrated within the website

---
+++ homepage/dist/daily_meal_plan/daily_plan.js
@@ -0,0 +1,48 @@
+// function GetCalories()
+// {
+//   <div className="app">
+//           <section className="controls">
+//              <input
+//                type="number"
+//                placeholder="Calories (e.g. 2000)"
+//                id="calories"
+//              />
+//              <button onclick="fetching();">Get Daily Meal Plan</button>
+//            </section>
+//             </div>
+// }
+function fetching(calories) {
+    fetch('https://api.spoonacular.com/mealplanner/generate?apiKey=de559a9360ca444db609c4915be035ca&timeFrame=day&targetCalories='+calories,)
+        .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setMealData(data)
+        const htmlholder = data.meals.map(meals =>{ 
+          return `
+          <div class= "meals">
+              <p> Title: ${meals.title}</p>
+              <p> Servings: ${meals.servings}</p>
+              <p> Summary: ${meals.readyInMinutes}</p>
+              <a href=${meals.sourceUrl}>Go to Recipe</a>
+          </div>
+          `}).join(" ");
+      const htmlhld = data.meals.extendedIngredients
+      console.log(htmlholder)
+      document.querySelector('#output').insertAdjacentHTML('afterbegin',htmlholder);})
+      
+}
+
+function setMealData(data)
+{
+    return`
+        <div class="nutrients">
+          <h1>Macros</h1>
+          <ul>
+            <li>Calories: ${data.title}</li>
+            <li>Carbohydrates: ${data.nutrients.carbohydrates}</li>
+            <li>Fat: ${data.nutrients.fat}</li>
+            <li>Protein: ${data.nutrients.protein}</li>
+          </ul>
+        </div>`;
+
+}
