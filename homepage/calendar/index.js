656230503a22ad4a5c53e86cef29131947e01d70
johndinh391 <johndinh391@csu.fullerton.edu>
2022-05-08 22:51:14 -0700

    Added Hyperlink to map and line break

--- homepage/calendar/index.js
+++ homepage/calendar/index.js
@@ -66,7 +66,8 @@ document.getElementById("app").innerHTML = `
     <div class="modal-body">
       <form name='popup_food' onsubmit="return false">
         <br> <b>Input Meal: </b><br><input type="text" id="food" value="(Blank)"> <br>
-        <a href="../meal_options/index.html">Don't know what to eat? Click here!</a> 
+        <a href="../meal_options/index.html">Don't know what to eat? Click here!</a> <br>
+        <a href="../map/index.html"> Have an address? Put it in our map!</a>
 
         <br><br> <b>Set Meal Time: </b><br> 
         Hour: 
